fb47d6b49c9241361277f07bc5303480642e8f19
João Maia <joaocarlosbgm@gmail.com>
2022-11-05 09:18:41 -0300

    feat: user login router + update .env example with database_url

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import userRoutes from "./routes/userRoutes.routes";
+import sessionRoutes from "./routes/sessionRoutes.routes";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(sessionRoutes);
 
 app.use(errorMiddleware);
 
