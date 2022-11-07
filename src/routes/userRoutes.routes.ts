f7fd044e698159c384cb411c1ec785d418288b52
=Welton Santos <weltonsantosfr@gmail.com>
2022-11-07 10:05:43 -0300

    hotfix: fix user routes, user utils and services

--- src/routes/userRoutes.routes.ts
+++ src/routes/userRoutes.routes.ts
@@ -3,21 +3,20 @@ import userCreateController from "../controllers/user/userCreate.controller";
 import userDeleteController from "../controllers/user/userDelete.controller";
 import userListController from "../controllers/user/userList.controller";
 import userUpdateController from "../controllers/user/userUpdate.controller";
-import userAlreadyDeletedUtils from "../utils/user/userAlreadyDeleted.utils";
 import userAlreadyExitsUtils from "../utils/user/userAlreadyExits.utils";
 import userNotFoundUtils from "../utils/user/userNotFound.utils";
 import userUpdatePasswordUtils from "../utils/user/userUpdatePassword.utils";
 
 const userRoutes = Router();
 
-userRoutes.post("/users", userAlreadyExitsUtils, userCreateController);
-userRoutes.get("/users", userListController);
+userRoutes.post("", userAlreadyExitsUtils, userCreateController);
+userRoutes.get("", userListController);
 userRoutes.patch(
-  "/users/:id",
+  "/:id",
   userNotFoundUtils,
   userUpdatePasswordUtils,
   userUpdateController
 );
-userRoutes.delete("/users/:id", userNotFoundUtils, userDeleteController);
+userRoutes.delete("/:id", userNotFoundUtils, userDeleteController);
 
 export default userRoutes;
