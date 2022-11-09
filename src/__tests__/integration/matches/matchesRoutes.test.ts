2833e596dfb59106ad10da81da88a46c4d34374b
João Maia <joaocarlosbgm@gmail.com>
2022-11-09 15:08:43 -0300

    feat: delete match tests

--- src/__tests__/integration/matches/matchesRoutes.test.ts
+++ src/__tests__/integration/matches/matchesRoutes.test.ts
@@ -147,4 +147,92 @@ describe("/matches", () => {
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(403);
   });
+
+  test("DELETE /matches/:id - Must be able to delete a match", async () => {
+    await request(app).post("/users").send(mockedAdmin);
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const token = `Bearer ${adminLoginResponse.body.token}`;
+
+    const createdMatch = await request(app)
+      .post("/matches")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedMatch);
+
+    const matchToBeDelete = await request(app).get("/matches");
+    const matchToBeDeleteId = matchToBeDelete.body[0].id;
+
+    const response = await request(app)
+      .delete(`/matches/${matchToBeDeleteId}`)
+      .set("Authorization", token);
+
+    expect(response.body).toEqual({});
+    expect(response.status).toBe(204);
+  });
+
+  test("DELETE /matches/:id - Shouldn't be able to delete a match without authentication", async () => {
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const token = `Bearer ${adminLoginResponse.body.token}`;
+
+    const createdMatch = await request(app)
+      .post("/matches")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedMatch);
+
+    const matchToBeDelete = await request(app).get("/matches");
+    const matchToBeDeleteId = matchToBeDelete.body[0].id;
+
+    const response = await request(app).delete(`/matches/${matchToBeDeleteId}`);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
+
+  test("DELETE /matches/:id - Should not be able to delete a match without admin permission", async () => {
+    await request(app).post("/users").send(mockedUser);
+    const userLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+    const token = `Bearer ${userLoginResponse.body.token}`;
+
+    const createdMatch = await request(app)
+      .post("/matches")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+      .send(mockedMatch);
+
+    const matchToBeDelete = await request(app).get("/matches");
+    const matchToBeDeleteId = matchToBeDelete.body[0].id;
+
+    const response = await request(app)
+      .delete(`/matches/${matchToBeDeleteId}`)
+      .set("Authorization", token);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(403);
+  });
+
+  test("DELETE /matches/:id - Should not be able to delete a match with wrong id", async () => {
+    await request(app).post("/users").send(mockedAdmin);
+    const adminLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedAdminLogin);
+    const token = `Bearer ${adminLoginResponse.body.token}`;
+
+    const createdMatch = await request(app)
+      .post("/matches")
+      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+      .send(mockedMatch);
+
+    const matchToBeDelete = await request(app).get("/matches");
+
+    const response = await request(app)
+      .delete("/matches/13970655-5dbe-423a-9a9d-5c23b37943cf")
+      .set("Authorization", token);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(404);
+  });
 });
